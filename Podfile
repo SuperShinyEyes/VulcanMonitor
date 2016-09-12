23836450497cc9ac51df8dcc7eaf46962800a68e
seyoungTango <seyoung.arts.park@protonmail.com>
2016-09-11 21:35:58 -0400

    Install Pods

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '9.0'
+inhibit_all_warnings!
+use_frameworks!
+
+def pods
+    pod 'Mapbox-iOS-SDK', '~> 3.3.1'
+    pod 'SwiftyBeaver' # Logging w/ colors
+    #    pod 'UIImageView-Letters'
+end
+
+
+target 'vulcanMonitor' do
+    pods
+end
+
---
+++ vulcanMonitor/MonitorController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  vulcanMonitor
+//
+//  Created by Park Seyoung on 11/09/16.
+//  Copyright © 2016 Park Seyoung. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MonitorController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
